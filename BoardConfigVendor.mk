47fc1fec86673567b8a9fb1603b16ccc74ca9e44
vember31 <prestigioustennis@gmail.com>
2026-05-10 16:55:47 -0500

    uke: Regenerate radio firmware package

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,29 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    countrycode \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    multiimgoem \
+    multiimgqti \
+    qupfw \
+    shrm \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
